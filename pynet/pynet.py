38fb260e05dcf185506338a7d4ead7f4af1ef0cd
Jim James <neron.academico@gmail.com>
2024-07-30 18:22:14 -0600

    update

---
+++ pynet/pynet.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+import argparse
+
+
+# Personal packages
+from utils.colors import Colors
+from args.connection import connect_to_ip, send_command, get_private_ip
+from args.backend import connect_to_database, save_ifconfig, exec_ifconfig, check_existing_record, det_ssid, save_ssid
+#from args.backend_asyn import exec_ifconfig, save_ifconfig
+
+
+# Function to parse command-line arguments
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="PyNet - A Python-based tool for telnet and network operations.")
+    parser.add_argument("-iL", "--ip_list", help="\t\tPath to a file containing a list of IP addresses or hostnames, one per line.")
+    parser.add_argument("-i", "--ip", help="\t\tTarget IP address or hostname.")
+    parser.add_argument("-p", "--port", default=23, type=int, help="\t\tTarget port (default is 23 for Telnet).")
+    parser.add_argument("-u", "--username", help="\t\tUsername for authentication.")
+    parser.add_argument("-pw", "--password", help="\t\tPassword for authentication.")
+    parser.add_argument("-l", "--login", action="store_true", help="\tSimply login on the target system.")
+    parser.add_argument("-s", "--ssid", action="store_true", help="\tDetermine the SSID name associated with each IP.")
+    parser.add_argument("-c", "--command", help="\tCommand to execute on the target system.")
+    return parser.parse_args()
+
+
+# Main function
+def main():
+
+    # Parse command-line arguments
+    args = parse_arguments()
+
+    # Connect to the database
+    conn = connect_to_database()
+    if conn is None:
+        sys.exit(0)
+
+    # Fetch the private ip of host running this python script
+    source_ip = get_private_ip()
+
+    # Check if -iL or -i is provided
+    if args.ip_list:
+        ip_list_path = args.ip_list
+        # Read the IP list file and iterate through the IP addresses
+        with open(ip_list_path, 'r') as file:
+            ip_addresses = file.read().splitlines()
+
+        ip_cont = 1
+        ip_cont_2 = 1
+        for ip in ip_addresses:
+
+            # Simply perform a telnet connection.
+            if args.login:
+                #print(Colors.BOLD_WHITE + f"\n[{ip_cont}] [{ip_cont_2}] Processing IP: {ip}" + Colors.R)
+                print(Colors.BOLD_WHITE + f"[{ip_cont}] Processing IP: {ip}" + Colors.R)
+                tn = connect_to_ip(conn, source_ip, ip, args.username, args.password, args.port) # Perform a telnet connection 
+
+            if args.ssid:
+                if tn:
+                    print(Colors.ORANGE + f"[!] Trying to determine the SSID name of {ip}" + Colors.R)
+                    command = "display wifi information"    # Command for extracting the SSID
+                    ssids = det_ssid(tn, command)
+                    save_ssid(conn, ip, ssids)     # Save ssids into database
+                else:
+                    print(Colors.RED + f"[#] There's not an existing connection to {ip}" + Colors.R)
+
+
+            #if not check_existing_record(conn, ip, args.command):
+                #pass
+                #print(f"\n[{ip_cont}] [{ip_cont_2}] Processing IP: {ip}")
+                #tn = connect_to_ip(conn, source_ip, ip, args.username, args.password, args.port) # Perform a telnet connection 
+
+                # Execute commands in sequence using the same connection
+                #exec_ifconfig (conn, ip, tn, args.command)
+                
+                # Create a connection pool
+                #pool = await create_pool()
+
+                # Asynchronous
+                #await exec_ifconfig(pool, ip, tn, command)
+
+            #else:
+                #print(f"\n[{ip_cont}] [{ip_cont_2}] Record already exists for IP:", ip)
+                #ip_cont_2 += 1
+
+            ip_cont += 1
+
+    elif args.ip:
+        print(f"\nProcessing IP: {args.ip}")
+        exec_ifconfig (conn, args.ip, tn, args.command)
+    else:
+        print("Error: Please provide either -iL or -i option to specify the target IP(s).")
+
+    if conn:
+        conn.close()
+        print(Colors.GREEN + "\n[!] Connection to DB closed properly." + Colors.GREEN)
+
+
+if __name__ == "__main__":
+    main()
