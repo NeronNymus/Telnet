93ebaeb6b381b1f292cfda77a8e1155c820d40f7
AndresNeron <neron.academico@gmail.com>
2024-09-29 01:45:32 -0600

    update

--- pynet/create_virtualenv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This code needs to be executed in the root directory of 'pynet'.
-
-virtualenv --python=3 "telEnv"
-source "telEnv/bin/activate"
-pip3 install -r requirements.txt
-
--- pynet/exports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DB_HOST='192.168.100.233'  
-export DB_PORT='2356'
-
-export DB_NAME='pynet'
-export DB_USER='grimaldi'
-export DB_PASSWORD='52a7cZdX'
